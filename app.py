bf3d88a79e0f1c016fe63b2c2719007517f23a6e
cynliu98 <cynliu98@mit.edu>
2017-09-16 22:42:09 -0400

    Final Cynthia edits

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
+def index():
+    return render_template('index.html')
+
 @app.route("/result")
 def result(result=None):
     return render_template('result.html', result=None)
-
 
 app.run('127.0.0.1', '5000')
