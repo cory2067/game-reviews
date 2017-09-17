660369dd4e3cfc07d3a9d6f17dad89bbbc8a6129
Cory Lynch <cjl2625@gmail.com>
2017-09-16 22:38:43 -0400

    Create madlib generator from db.

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import json
 import random
 from textblob import TextBlob
 
-# the db has 231780 entries 
+# the db has ~20038 entries
 db = []
 
 print("Loading db...")
@@ -12,12 +12,40 @@ with open('db.json') as f:
 
 print("Done.")
 
-text = db[157630]['reviewText']
+review_id = 16578
+# review_id = random.randint(0, 20037)
+text = db[review_id]['reviewText']
 words = TextBlob(text).tags
 
-for w in words:
-    if w[1] in ('NN','NNS','NNP','NNPS','JJ','JJR','JJS','VBD','VBG'):
-        print(w)
+payload = {'review': review_id}
+libs = []
 
+def add(i, w, pos):
+    libs.append({
+        'id': i,
+        'word': w,
+        'pos': pos,
+    })
 
+for i,(w,pos) in enumerate(words):
+    if random.random() > 0.4:
+        continue
 
+    if pos == 'NN':
+        add(i,w,'Noun')
+    elif pos == 'NNS':
+        add(i,w,'Plural Noun')
+    elif pos == 'JJ':
+        add(i,w,'Adjective')
+    elif pos == 'JJR':
+        add(i,w,'-er Adjective')
+    elif pos == 'JJS':
+        add(i,w,'-est Adjective')
+    elif pos == 'VBD':
+        add(i,w,'Past Tense Verb')
+    elif pos == 'VGB':
+        add(i,w,'Gerund (Verb-ing)')
+
+payload['words'] = libs
+
+print(payload)
